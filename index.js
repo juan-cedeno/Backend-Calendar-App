717135a09dad8491e9745133b964b8e28296bb96
Juan Julio Cedeño <71097019+juan-cedeno@users.noreply.github.com>
2021-02-03 20:44:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,13 @@ const cors = require('cors')
 
 const { databaseConection } = require('./database/config')
 
-
-
-
 const app = express()
 app.use(cors())
 databaseConection()
 app.use(express.json())
 
-
 app.use('/api/auth/', require('./routers/auth'))
 app.use('/api/events/', require('./routers/events'))
-
 
 app.listen(process.env.PORT, () => {
     console.log(`LISTEN ${process.env.PORT}`);
